6eaf39e5bd04ccd38c9d11cf6cefa89574712487
Nala <afiiqotulula@gmail.com>
2026-06-14 23:51:13 +0700

    Fix: Merapikan indentasi spasi di app.py baris 54

--- app.py
+++ app.py
@@ -51,15 +51,15 @@ def index():
         
         return render_template('response.html', kategori=kategori, laporan_secure=laporan_secure)
 
-   conn = sqlite3.connect(DB_FILE)
-   cursor = conn.cursor()
-   cursor.execute("SELECT id, kategori, laporan_terenkripsi, waktu FROM aduan ORDER BY id DESC")
-   daftar_aduan = cursor.fetchall()
-   conn.close()
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, kategori, laporan_terenkripsi, waktu FROM aduan ORDER BY id DESC")
+    daftar_aduan = cursor.fetchall()
+    conn.close()
     
-   return render_template('form.html', aduan_data=daftar_aduan)
+    return render_template('form.html', aduan_data=daftar_aduan)
 
 if __name__ == '__main__':
-   init_db()
-   port = int(os.environ.get("PORT", 5000))
-   app.run(host='0.0.0.0', port=port)
+    init_db()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
